08b5a33df5f8b7fcfc16fba09dca69f099ac5bbb
Billy Vong <billy@sentry.io>
2018-08-14 09:45:08 -0700

    fix tests

--- src/sentry/static/sentry/app/views/organizationHealth/util/healthRequest.jsx
+++ src/sentry/static/sentry/app/views/organizationHealth/util/healthRequest.jsx
@@ -15,6 +15,7 @@ class HealthRequestWithParams extends React.Component {
     /**
      * API client instance
      */
+<<<<<<< HEAD
     api: PropTypes.object.isRequired,
 
     organization: SentryTypes.Organization.isRequired,
@@ -24,10 +25,16 @@ class HealthRequestWithParams extends React.Component {
      */
     tag: PropTypes.string.isRequired,
 
+=======
+    api: PropTypes.object,
+
+    organization: SentryTypes.Organization.isRequired,
+
+>>>>>>> dbc5104210... fix tests
     /**
-     * Callback function to process category
+     * Health tag (this will use a BASE_URL defined in health actionCreators
      */
-    getCategory: PropTypes.func,
+    tag: PropTypes.string.isRequired,
 
     /**
      * List of project ids to query
